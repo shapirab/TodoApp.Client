d18316ac8165afeb341325d47e4ac897175e008d
shapirab <shapirab@jamd.ac.il>
2025-08-14 20:19:12 +0300

    14/8/25 fixed ssr issues

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,7 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: 'todo/:id', renderMode: RenderMode.Server },
   {
     path: '**',
     renderMode: RenderMode.Prerender
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TodoDetailComponent } from './features/todo/todo-detail/todo-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TodoDetailComponent, pathMatch: 'full' },
+  { path: 'todo/:id', component: TodoDetailComponent },
+  { path: '**', redirectTo: '' }
+];
