906c0569e8422a66f8dcd74379a984e195bf6257
shapirab <shapirab@jamd.ac.il>
2025-10-29 19:49:47 +0200

    29/10/25 developed todoList tamplate without yet refactoring todoDetail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { TodoDetailComponent } from './features/todo/todo-detail/todo-detail.component';
+import { TodoListComponent } from './features/todo/todo-list/todo-list.component';
 
 export const routes: Routes = [
-  { path: '', component: TodoDetailComponent, pathMatch: 'full' },
+  { path: '', component: TodoListComponent, pathMatch: 'full' },
+  { path: 'todo', component: TodoListComponent },
   { path: 'todo/:id', component: TodoDetailComponent },
   { path: '**', redirectTo: '' }
 ];
