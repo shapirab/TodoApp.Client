4100f2b1db963cf5437b3364e32e8314236b57b6
shapirab <shapirab@jamd.ac.il>
2025-11-11 15:08:07 +0200

    11/11/25 added home, login and register components. Added accoutService

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { TodoDetailComponent } from './features/todo/todo-detail/todo-detail.component';
 import { TodoListComponent } from './features/todo/todo-list/todo-list.component';
+import { HomeComponent } from './features/home/home.component';
+import { LoginComponent } from './features/login/login.component';
+import { RegisterComponent } from './features/register/register.component';
 
 export const routes: Routes = [
-  { path: '', component: TodoListComponent, pathMatch: 'full' },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'todo', component: TodoListComponent },
   { path: 'todo/:id', component: TodoDetailComponent },
   { path: '**', redirectTo: '' }
---
+++ src/app/core/services/account.service.ts
@@ -0,0 +1,56 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { UserToReturn } from '../models/user/userToReturn';
+import { HttpClient } from '@angular/common/http';
+import { Login } from '../models/user/login';
+import { switchMap, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl: string = environment.apiUrl;
+  currentUser = signal<UserToReturn | null | undefined>(undefined);
+
+  private http = inject(HttpClient);
+
+  login(values: any) {
+    return this.http.post<Login>(`${this.baseUrl}/accounts/login`, values, {
+        withCredentials: true,
+        observe: 'response',
+      })
+      .pipe(
+        switchMap(() => this.getUserInfo())
+      );
+  }
+
+  register(values: any) {
+    return this.http.post(`${this.baseUrl}/accounts/register`, values, {withCredentials: true});
+  }
+
+  getUserInfo() {
+    return this.http.get<UserToReturn>(`${this.baseUrl}/accounts/user-info`, {
+        withCredentials: true,
+      })
+      .pipe(
+        tap((user) => {
+          if (user) {
+            //this.setUserHintCookie(user); // keep cookie in sync
+            this.currentUser.set(user);
+          } else {
+            //this.setUserHintCookie(null);
+            this.currentUser.set(null);
+          }
+        })
+      );
+  }
+
+   private setUserHintCookie(email: string | null) {
+    const name = 'bb_user';
+    document.cookie = email
+      ? `${name}=${encodeURIComponent(
+          email
+        )}; Path=/; Max-Age=2592000; SameSite=None; Secure`
+      : `${name}=; Path=/; Max-Age=0; SameSite=None; Secure`;
+  }
+}
