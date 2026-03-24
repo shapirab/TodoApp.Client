2b169780a1b83ad4a0f5d97f3adfe8e5bddf3d06
shapirab <shapirab@jamd.ac.il>
2026-03-24 19:54:59 +0200

    developed the register component

--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AccountService } from '../../core/services/account.service';
+import { Router } from '@angular/router';
+import { Register } from '../../core/models/user/register';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  private fb = inject(FormBuilder);
+  private accountService = inject(AccountService);
+  private router = inject(Router);
 
+  registerForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  onRegister(){
+    let register: Register = {
+      firstName: this.registerForm.value.firstName ?? '',
+      lastName: this.registerForm.value.lastName ?? '',
+      email: this.registerForm.value.email ?? '',
+      password: this.registerForm.value.password ?? '',
+    }
+
+    this.accountService.register(register).subscribe({
+      next: res => {
+        //this.accountService.currentUser.set()
+        this.router.navigateByUrl('/todo');
+      },
+      error: err => console.log(err)
+    });
+  }
 }
